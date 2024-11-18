193cb6667b04806e9fe6fb27fac6bfd0a6ca13b3
Mehedi Hasan <mehedi@xinaps.com>
2024-11-18 12:44:14 +0600

    javascript example

---
+++ src/views/JavascriptExample.vue
@@ -0,0 +1,33 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "JavascriptExample",
+  data() {
+    return {
+      x: null
+    }
+  },
+  methods: {
+    test() {
+      this.x.forEach(it => {
+        console.log(it)
+      })
+      this.x.find(it => it == null);
+      this.x = "";
+      this.x = {
+        name: "Javascript",
+      }
+      this.x = ["a", "b", "c", "d"];
+    }
+  }
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
